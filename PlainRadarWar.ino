024e9694e1cef4bc0b4d17af187eeb3d4392b031
iserlown-y <iserlown@yahoo.co.jp>
2022-05-15 10:01:30 +0900

    battleship -> PlainRadarWar タイトルにバージョン表示 README.md記載

--- PlainRadarWar.ino
+++ PlainRadarWar.ino
@@ -1,29 +1,4 @@
 /*
- * MIT License
- * 
- * Begin license text.
- * 
- * Copyright 2022 Atsuro Ida
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
- * following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all copies or substantial
- * portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- * 
- * End license text.
- * 
- * 
  * Plain RaderWar ver1.0.0
  * Arduboy用のレーダー作戦ゲーム
  */
@@ -109,11 +84,12 @@ void reStart(short phase_)
 bool showTitle()
 {
   bool bres = false;
-  arduboy.setCursor(0,10);
+  arduboy.setCursor(0,0);
   arduboy.setTextSize(2);
   arduboy.println("Plain");
   arduboy.println("RadarWar");
   arduboy.setTextSize(1);
+  arduboy.println("v1.0.0");
   arduboy.setCursor(0,48);
   arduboy.println(" Press [A] sound.on");
   arduboy.println(" Press [B] sound.off");
